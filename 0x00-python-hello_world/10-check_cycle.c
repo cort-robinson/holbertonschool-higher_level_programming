eb90ad73ae73588f4eb6de90ef8919e6a439b173
cort-robinson <cortrobinson99@gmail.com>
2020-09-01 00:34:28 +0000

    add 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks for cycle in singly linked list.
+ * @list: List to check.
+ * Return: 0 if no cycle, else 1.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *check, *head;
+
+	head = list;
+	check = list;
+
+	while (check != NULL && check->next != NULL)
+	{
+			while (head != NULL && head->next != NULL)
+			{
+				if (head == check)
+					return (1);
+				head = head->next;
+			}
+		check = check->next;
+	}
+	return (0);
+}
