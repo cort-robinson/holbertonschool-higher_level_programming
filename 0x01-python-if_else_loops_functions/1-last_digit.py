47ab2dde510fdf6cb5764cef75ef2ab0801442b6
cort-robinson <cortrobinson99@gmail.com>
2020-09-02 06:43:38 +0000

    format change

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     last_digit = number % -10
 else:
     last_digit = number % 10
-print(last_digit, end=" ")
+print("{:d}".format(last_digit), end=" ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
