eeed86549412ddc2388be990ec4067a9007a58d4
cort-robinson <cortrobinson99@gmail.com>
2020-09-02 02:56:59 +0000

    Fix last digit calc

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 print("Last digit of {:n} is".format(number), end=" ")
-lastDigit = abs(number % 10)
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = number % -10
 print(lastDigit, end=" ")
 if lastDigit > 5:
     print("and is greater than 5")
 elif lastDigit == 0:
     print("and is 0")
-else:
+elif lastDigit < 6 and lastDigit != 0:
     print("and is less than 6 and not 0")
