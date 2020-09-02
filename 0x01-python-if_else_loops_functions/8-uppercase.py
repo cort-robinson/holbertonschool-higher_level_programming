314b0c9997230cbbf8ec8e6a34117f7fccac660a
cort-robinson <cortrobinson99@gmail.com>
2020-09-02 04:22:36 +0000

    add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strout = ""
+    for i in str:
+        i = ord(i)
+        if 97 <= i <= 122:
+            i -= 32
+        strout += chr(i)
+    print(strout)
