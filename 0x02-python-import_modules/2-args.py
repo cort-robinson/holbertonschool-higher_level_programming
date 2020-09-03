3701e8f0d56b013266f8e6bede1f4f24a61b70c1
cort-robinson <cortrobinson99@gmail.com>
2020-09-03 19:36:24 +0000

    add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+argcount = len(argv) - 1
+print("{:d} argument".format(argcount), end="")
+if argcount != 1:
+    print("s." if argcount == 0 else "s")
+else:
+    print(":")
+if argcount > 0:
+    i = 1
+    while i <= argcount:
+        print("{:d}: {:s}".format(i, argv[i]))
+        i += 1
