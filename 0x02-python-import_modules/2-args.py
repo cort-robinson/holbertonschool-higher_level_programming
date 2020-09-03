99087dac100bb928c243d9d8b92992afe3471976
cort-robinson <cortrobinson99@gmail.com>
2020-09-03 19:37:53 +0000

    add main check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-argcount = len(argv) - 1
-print("{:d} argument".format(argcount), end="")
-if argcount != 1:
-    print("s." if argcount == 0 else "s")
-else:
-    print(":")
-if argcount > 0:
-    i = 1
-    while i <= argcount:
-        print("{:d}: {:s}".format(i, argv[i]))
-        i += 1
+if __name__ == "__main__":
+    from sys import argv
+    argcount = len(argv) - 1
+    print("{:d} argument".format(argcount), end="")
+    if argcount != 1:
+        print("s." if argcount == 0 else "s")
+    else:
+        print(":")
+    if argcount > 0:
+        i = 1
+        while i <= argcount:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
