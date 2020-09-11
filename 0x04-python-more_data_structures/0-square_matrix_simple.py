547ebf442842f4dbc5c60f9aa126e644674a71e6
cort-robinson <cortrobinson99@gmail.com>
2020-09-11 04:52:44 +0000

    change += to .append

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     result = []
     for i in matrix:
-        result += list(filter(lambda x: x * x, i))
+        result.append(list(map(lambda x: x * x, i)))
     return result
