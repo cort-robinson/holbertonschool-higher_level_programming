201a3d831744daeb05b8bbd923114301d37f5441
cort-robinson <cortrobinson99@gmail.com>
2020-09-11 05:51:54 +0000

    add file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def value(n):
+    if n == 'I':
+        return 1
+    if n == 'V':
+        return 5
+    if n == 'X':
+        return 10
+    if n == 'L':
+        return 50
+    if n == 'C':
+        return 100
+    if n == 'D':
+        return 500
+    if n == 'M':
+        return 1000
+    return -1
+
+def roman_to_int(roman_string):
+    sum = 0
+    for i in range(len(roman_string)):
+        current_v = value(roman_string[i])
+        if i + 1 < len(roman_string):
+            if roman_string[i + 1]:
+                next_v = value(roman_string[i + 1])
+            if current_v < next_v:
+                sum += next_v - current_v
+                i += 1
+                continue
+        sum += current_v
+    return sum
