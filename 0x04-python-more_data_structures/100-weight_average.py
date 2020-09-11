33670690756cf063a49d98d26154921c9e2fdb5b
cort-robinson <cortrobinson99@gmail.com>
2020-09-11 14:24:12 +0000

    fix empty list check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return 0
     total_weight = sum(list(i[1] for i in my_list))
     sum_total = sum(i[0] * i[1] for i in my_list)
