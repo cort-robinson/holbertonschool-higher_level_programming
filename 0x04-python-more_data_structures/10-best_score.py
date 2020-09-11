7ff47e51053de52dcdddc4aa7f40fb45fac1e911
cort-robinson <cortrobinson99@gmail.com>
2020-09-11 05:07:54 +0000

    add file

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    largest = 0
+    if a_dictionary is not None:
+        for key, value in a_dictionary.items():
+            if value > largest:
+                largest = value
+                largest_key = key
+        return largest_key
