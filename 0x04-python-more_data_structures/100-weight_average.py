7277658ccec906c7d74737bad1f617f6745c1942
cort-robinson <cortrobinson99@gmail.com>
2020-09-11 06:55:32 +0000

    pep8 compliance

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    total_weight =  sum(list(i[1] for i in my_list))
+    total_weight = sum(list(i[1] for i in my_list))
     sum_total = sum(i[0] * i[1] for i in my_list)
     return sum_total / total_weight
