258106b3f6f0830e3f9d5af2ce70b1b9cbdaa315
cort-robinson <cortrobinson99@gmail.com>
2020-09-11 05:21:40 +0000

    add key initialization

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     largest = 0
+    largest_key = None
     if a_dictionary is not None:
         for key, value in a_dictionary.items():
             if value > largest:
                 largest = value
                 largest_key = key
-        return largest_key
+    return largest_key
