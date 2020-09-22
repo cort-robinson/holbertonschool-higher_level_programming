bfdef0dc24c7dab717199ecdcfcc89d6ae44feb5
cort-robinson <cortrobinson99@gmail.com>
2020-09-22 09:56:35 +0000

    add file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Make a class Square that defines a square
+by: (based on 0-square.py)'''
+
+
+class Square:
+    '''This is a square class'''
+    def __init__(self, size=0):
+        '''Initialize instance attribute size'''
+        self.__size = size
