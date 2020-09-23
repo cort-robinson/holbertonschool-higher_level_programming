21b0f6e09e90bcd6f35ea06d9ab09a2c2dcf3be2
cort-robinson <cortrobinson99@gmail.com>
2020-09-23 04:43:59 +0000

    fix my_print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,10 +46,11 @@ class Square:
 
     def my_print(self):
         """Print the Square instance to stdout"""
-        if self.__position[1] > 0:
-            print('\n' * self.__position[1], end="")
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print('#' * self.__size)
         if self.__size == 0:
             print()
+        else:
+            if self.__position[1] > 0:
+                print('\n' * self.__position[1], end="")
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print('#' * self.__size)
