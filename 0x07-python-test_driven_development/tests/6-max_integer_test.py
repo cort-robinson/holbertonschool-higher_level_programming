15195ed7572eb772d90bb0ad9ce9ff775e00d79e
cort-robinson <cortrobinson99@gmail.com>
2020-09-30 15:02:46 +0000

    fix test arguments

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,9 +15,9 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_returnval(self):
         """Test argument types"""
-        self.assertEqual(max_integer([1, 2, 3, 4], 4))
-        self.assertEqual(max_integer([1, 3, 4, 2], 4))
-        self.assertEqual(max_integer([-1, -2, -3, -4], -1))
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer(), None)
 
