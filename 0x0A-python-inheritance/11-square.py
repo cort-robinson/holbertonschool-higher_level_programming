7b696910651b392dadf8fa4c7d76a22c14daa83d
cort-robinson <cortrobinson99@gmail.com>
2020-10-06 23:04:58 +0000

    add file

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializer"""
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns area of Square instance"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Informal representation of Square instance"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
