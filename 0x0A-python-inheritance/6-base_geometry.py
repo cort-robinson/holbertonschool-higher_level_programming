f70e611c4502d204eea1af304eccc198473bac6b
cort-robinson <cortrobinson99@gmail.com>
2020-10-06 22:12:44 +0000

    add file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Contains unfinished area method"""
+    def area(self):
+        raise Exception("area() is not implemented")
