713da6965991929a3077d96c17419c2d91d77bdb
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 00:24:25 +0000

    check if file exists

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,5 +5,9 @@ from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = load_from_json_file('add_item.json')
+
+try:
+    my_list = load_from_json_file('add_item.json')
+except:
+    my_list = []
 save_to_json_file(my_list + argv[1:], 'add_item.json')
