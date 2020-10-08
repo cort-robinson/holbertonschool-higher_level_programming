b38be9a5002add17a5562428a20147fe9e8ee1a9
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 00:58:00 +0000

    add file

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines class Student"""
+
+
+class Student:
+    """Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs == None:
+            return self.__dict__
+        my_dict = {}
+        for _ in attrs:
+            for k, v in self.__dict__.items():
+                if k == _:
+                    my_dict.update({k: v})
+        return my_dict
