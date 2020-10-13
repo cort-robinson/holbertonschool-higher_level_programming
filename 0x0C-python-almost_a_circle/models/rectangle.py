2cf28418f82300abfa653017d98054101ffe0e89
cort-robinson <cortrobinson99@gmail.com>
2020-10-13 08:14:18 +0000

    add to_dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,7 @@ class Rectangle(Base):
         else:
             for k, v in kwargs.items():
                 setattr(self, k, v)
+
+    def to_dictionary(self):
+        return {"id": self.id, "width": self.width,
+                "height": self.height, "x": self.x, "y": self.y}
