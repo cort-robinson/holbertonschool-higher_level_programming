6fc768e29e15e70780fcdd17269d17e2dc21d935
cort-robinson <cortrobinson99@gmail.com>
2020-11-17 10:36:45 +0000

    add file

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all genres from hbtn_0d_tvshows and displays number of shows linked to each
+SELECT tv_genres.name,
+    COUNT(*) AS number_of_shows
+FROM tv_genres
+    LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC,
+    tv_genres.id ASC;
