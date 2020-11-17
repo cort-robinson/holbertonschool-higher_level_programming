d55c15f26c967768b41c08dd703242630a8c2e9e
cort-robinson <cortrobinson99@gmail.com>
2020-11-17 11:44:44 +0000

    add file

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- Lists all Comedy shows in hbtn_0d_tvshows
+SELECT s.title
+FROM tv_shows AS s
+    JOIN tv_show_genres AS sg ON s.id = sg.show_id
+    JOIN tv_genres AS g ON sg.genre_id = g.id WHERE g.name = 'Comedy'
+ORDER BY s.title;
