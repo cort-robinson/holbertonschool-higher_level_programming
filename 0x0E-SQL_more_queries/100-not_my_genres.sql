f381cf78814de30edd3c046de5d5c054aa7a7168
cort-robinson <cortrobinson99@gmail.com>
2020-11-17 12:27:46 +0000

    add file

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+SELECT g.name
+FROM tv_genres AS g
+WHERE g.name NOT IN (
+        SELECT g.name
+        FROM tv_genres AS g
+            JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+            JOIN tv_shows AS s ON sg.show_id = s.id
+        WHERE s.title = "Dexter"
+    )
+ORDER BY g.name;
