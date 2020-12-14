27922afa0d771e30fa0667c77c9b6abd5bb48894
cort-robinson <cortrobinson99@gmail.com>
2020-12-14 02:48:05 +0000

    pep8 compliance

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""
+Lists all states with a name starting
+with N from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 from sys import argv
 
