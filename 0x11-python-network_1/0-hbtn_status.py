76e5a187f4c14d614ee94903d857aa38027f74ab
cort-robinson <cortrobinson99@gmail.com>
2021-01-12 04:32:29 +0000

    add file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as req:
+        html = req.read()
+        print('Body ressponse:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
