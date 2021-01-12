8ca13ff139ae56d4230ef5dab9bd103716445cb4
cort-robinson <cortrobinson99@gmail.com>
2021-01-12 04:35:21 +0000

    revert previous change and fix typo in print()

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
-import urllib.request as request
+from urllib import request
 
 if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as req:
         html = req.read()
-        print('Body ressponse:')
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
