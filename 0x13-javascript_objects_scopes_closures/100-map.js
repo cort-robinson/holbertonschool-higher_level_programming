3566326a996e746a6669190072b2b7eec160289a
cort-robinson <cortrobinson99@gmail.com>
2021-02-09 01:38:38 +0000

    add file

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-const newList = [];
-for (const index in list) {
-  newList.push(list[index] * index);
-}
+const newList = list.map((num, index) => num * index);
 console.log(list);
 console.log(newList);
