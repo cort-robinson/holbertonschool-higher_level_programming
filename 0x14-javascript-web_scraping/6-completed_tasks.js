2b727798272666b1f769f50adcffee0e7f47f652
cort-robinson <cortrobinson99@gmail.com>
2021-02-09 22:15:59 +0000

    semistandard compliance

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,9 +3,11 @@
 const request = require('request');
 
 request(process.argv[2], (err, res, body) => {
-  tasks = JSON.parse(body).filter(task => task.completed === true);
+  if (err) console.log(err);
+
+  const tasks = JSON.parse(body).filter(task => task.completed === true);
   console.log(tasks);
-  let dict = {};
+  const dict = {};
   for (const task of tasks) {
     if (dict[task.userId] === undefined) {
       dict[task.userId] = 1;
