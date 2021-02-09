1ad90332e7974a4797eb64e4bada30ee2d52ff5e
cort-robinson <cortrobinson99@gmail.com>
2021-02-09 00:53:18 +0000

    add file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const baseSquare = require('./5-square');
+module.exports = class Square extends baseSquare {
+  charPrint (c = 'X') {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
