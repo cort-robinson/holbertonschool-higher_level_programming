d79b42e8c9282f82fd6712dbe72f78384e2f976d
cort-robinson <cortrobinson99@gmail.com>
2021-02-09 21:03:50 +0000

    add file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) console.log(error);
+
+  const data = JSON.parse(body).results;
+  const films = data.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  console.log(films.length);
+});
