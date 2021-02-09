ef174a8cf631c0d7d0949fb893137879121c6660
cort-robinson <cortrobinson99@gmail.com>
2021-02-09 19:49:13 +0000

    add error handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,6 @@
 const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
+  if (error) console.log(error);
   console.log('code:', response && response.statusCode);
 });
