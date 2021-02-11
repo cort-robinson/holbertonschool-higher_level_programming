349148379419da1f4ee61152a4ecd3cdb0e740b0
cort-robinson <cortrobinson99@gmail.com>
2021-02-11 22:27:52 +0000

    add file

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    $.each(data.results, (index, film) => $('UL#list_movies').append('<li>' + film.title + '</li>'));
+  });
+});
