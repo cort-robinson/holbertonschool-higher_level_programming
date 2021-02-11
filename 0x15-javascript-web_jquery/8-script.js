5352e0f53cff900a4435e026d781bf39b823285b
cort-robinson <cortrobinson99@gmail.com>
2021-02-11 22:30:10 +0000

    update syntax

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', data => {
     $.each(data.results, (index, film) => $('UL#list_movies').append('<li>' + film.title + '</li>'));
   });
 });
